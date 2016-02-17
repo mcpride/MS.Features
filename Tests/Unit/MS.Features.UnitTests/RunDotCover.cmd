77e9e7e0ee8743027068d8a1a3c96a68f6c28414
M. Stolze <mcpride@gmx.de>
2016-02-17 21:15:01 +0100

    file deleted

--- Tests/Unit/MS.Features.UnitTests/RunDotCover.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-
-set toolFilePath=c:\TeamCity\buildAgent\tools\dotCover\dotCover.exe
-set vsTestFilePath=C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe
-set workingDir=.
-set testAssembly=c:\Projects\Rhenus.HD.Features.Solution\Tests\Unit\Rhenus.HD.Features.UnitTests\bin\Debug\Rhenus.HD.Features.UnitTests.dll
-
-%toolFilePath% analyse /ReportType=HTML /Output=dotCover.html "/TargetExecutable=%vsTestFilePath%" "/TargetWorkingDir=%workingDir%" "/TargetArguments=%testAssembly%"
